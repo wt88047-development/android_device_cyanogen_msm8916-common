6e711a5929b8c27cdce34c62dc86fc4133055610
karthikkm007 <jacksparrow941994@gmail.com>
2019-05-10 16:53:49 +0530

    wt88047: remove ims_rtp_daemon
    Signed-off-by: karthikkm007 <jacksparrow941994@gmail.com>

--- fs_config/file_caps.fs
+++ fs_config/file_caps.fs
@@ -49,17 +49,17 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[vendor/bin/ims_rtp_daemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_RADIO
-caps: NET_BIND_SERVICE
+#[vendor/bin/ims_rtp_daemon]
+#mode: 0755
+#user: AID_SYSTEM
+#group: AID_RADIO
+#caps: NET_BIND_SERVICE
 
-[system/vendor/bin/ims_rtp_daemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_RADIO
-caps: NET_BIND_SERVICE
+#[system/vendor/bin/ims_rtp_daemon]
+#mode: 0755
+#user: AID_SYSTEM
+#group: AID_RADIO
+#caps: NET_BIND_SERVICE
 
 [vendor/bin/imsrcsd]
 mode: 0755
